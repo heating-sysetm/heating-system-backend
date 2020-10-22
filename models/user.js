ea686c07fae61832487ab477c4d439c3c8db4200
alinadir <alinadirkhanlo.98@gmail.com>
2020-10-22 06:27:02 -0400

    create user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define("User", {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    isAdmin: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN,
+      defaultValue:false
+    },
+  });
+  return User;
+};
