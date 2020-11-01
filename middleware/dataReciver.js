95fd4b3c946a0250374d63ea32cfeed7e901c6f0
alinadir <alinadirkhanlo.98@gmail.com>
2020-11-01 08:57:07 -0500

    change some methods

--- middleware/dataReciver.js
+++ middleware/dataReciver.js
@@ -1,12 +1,35 @@
 let temperature = require("../controllers/temperature-controller.js");
 let gaz = require("../controllers/gaz-controller.js");
 
+
+async function addTemp(data){
+  let result='';
+  data.forEach(temp => {
+     result =temperature.addTemperature(data);
+   });
+   return result;
+}
+
 exports.saveData = async function (message) {
   if (message.type === "utf8") {
     data = JSON.parse(message.utf8Data);
-    console.log("---- recived from esp ---------");
-    
-    let result =await temperature.addTemperature(data);
+    let result='';
+    if(data.GasSensor_situation){
+      result = await gaz.addGaz(data.GasSensor_situation);
+    }else if (data.inside_situation) {
+        let insideTemps = data.inside_situation;
+        result=await addTemp(insideTemps);
+    } else if(data.out_situation){
+      let outside_temps=data.out_situation;
+      outside_temps.forEach(temp =>{
+        // result = await temperature.addTemperature(data);
+        console.log('outside temp added');
+        
+      });
+    }
     return result;
   }
 };
+
+
+
